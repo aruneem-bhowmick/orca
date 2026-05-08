53ecffca8eac506caf97472a5937ed19c69a9e15
Aruneem Bhowmick <aruneem.bhowmick@gmail.com>
2026-05-08 15:55:11 -0500

    feat(orcamind/embedders): export NeuralEmbedder and FaissIndex from embedders package

--- packages/orcamind/orcamind/embedders/__init__.py
+++ packages/orcamind/orcamind/embedders/__init__.py
@@ -1,1 +1,7 @@
 """Task embedders: statistical feature extraction and neural embedding."""
+
+from .neural import NeuralEmbedder
+from .similarity import FaissIndex
+from .statistical import StatisticalEmbedder
+
+__all__ = ["FaissIndex", "NeuralEmbedder", "StatisticalEmbedder"]
