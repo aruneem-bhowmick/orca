8b41fd1cd6cf4c99cfded0b73794779c0d9e240d
Aruneem Bhowmick <aruneem.bhowmick@gmail.com>
2026-05-16 09:52:07 -0500

    feat: export EvolutionarySearch from the search module
    Adds EvolutionarySearch to orcalab.search.__init__ so it is importable
    alongside the other strategies and appears in __all__.

--- packages/orcalab/orcalab/search/__init__.py
+++ packages/orcalab/orcalab/search/__init__.py
@@ -2,8 +2,9 @@
 
 from orcalab.search.base import SearchStrategy
 from orcalab.search.bayesian import BayesianSearch
+from orcalab.search.evolutionary import EvolutionarySearch
 from orcalab.search.grid_search import GridSearch
 from orcalab.search.meta_informed import MetaInformedSearch
 from orcalab.search.random_search import RandomSearch
 
-__all__ = ["BayesianSearch", "GridSearch", "MetaInformedSearch", "RandomSearch", "SearchStrategy"]
+__all__ = ["BayesianSearch", "EvolutionarySearch", "GridSearch", "MetaInformedSearch", "RandomSearch", "SearchStrategy"]
