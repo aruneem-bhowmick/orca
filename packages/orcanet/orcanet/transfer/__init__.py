3f53bae2d03de870fc300847131b42d469ed7f1c
Aruneem Bhowmick <aruneem.bhowmick@gmail.com>
2026-05-21 11:59:05 -0500

    feat(transfer): export WeightTransfer and get_optimizer_with_layer_lr

--- packages/orcanet/orcanet/transfer/__init__.py
+++ packages/orcanet/orcanet/transfer/__init__.py
@@ -3,5 +3,13 @@
 from orcanet.transfer.base import TransferStrategy
 from orcanet.transfer.feature_transfer import FeatureTransfer, linear_cka
 from orcanet.transfer.types import TransferScore
+from orcanet.transfer.weight_transfer import WeightTransfer, get_optimizer_with_layer_lr
 
-__all__ = ["FeatureTransfer", "TransferScore", "TransferStrategy", "linear_cka"]
+__all__ = [
+    "FeatureTransfer",
+    "TransferScore",
+    "TransferStrategy",
+    "WeightTransfer",
+    "get_optimizer_with_layer_lr",
+    "linear_cka",
+]
