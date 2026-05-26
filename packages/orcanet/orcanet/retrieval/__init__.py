4b7e4cdc0b591ae2b9e4e03f376ca3a84bed77b4
Aruneem Bhowmick <aruneem.bhowmick@gmail.com>
2026-05-26 09:45:05 -0500

    feat(retrieval): implement HybridRetriever three-stage pipeline

--- packages/orcanet/orcanet/retrieval/__init__.py
+++ packages/orcanet/orcanet/retrieval/__init__.py
@@ -2,5 +2,6 @@
 
 from orcanet.retrieval.query_expander import QueryExpander
 from orcanet.retrieval.ranker import LLMRanker
+from orcanet.retrieval.retriever import HybridRetriever
 
-__all__ = ["QueryExpander", "LLMRanker"]
+__all__ = ["QueryExpander", "LLMRanker", "HybridRetriever"]
